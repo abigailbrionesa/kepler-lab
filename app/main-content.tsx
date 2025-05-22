908421a4d94e95a6cab92ca4aa002a8c09a24f47
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-05-22 11:05:03 -0500

    refactor maincontent

---
+++ app/main-content.tsx
@@ -0,0 +1,24 @@
+"use client"
+import { useSelectedCategory } from "@/context/selected-category-context"
+import SolarSystem from "@/components/3d/solar-system"
+import { ControlPanel } from "@/components/panel/control-panel"
+import { SiteHeader } from "@/components/panel/site-header"
+import type { Planet } from "@prisma/client"
+
+export default function MainContent(planets_data: Planet[]) {
+  const { selectedCategory } = useSelectedCategory()
+
+  return (
+    <div className="flex flex-1 flex-col">
+      <SiteHeader />
+      <div className="flex flex-1 relative">
+        {selectedCategory && <ControlPanel />}
+        <div className="flex flex-1">
+          <div className="flex h-[calc(100vh-80px)] w-full items-center justify-center rounded-lg border border-dashed">
+            <SolarSystem {...planets_data} />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
