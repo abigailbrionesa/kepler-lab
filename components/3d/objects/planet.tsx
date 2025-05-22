84a5fc1cf506127a886c7ecc2963dd2aebfa421a
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-05-22 15:32:01 -0500

    fix types and add orbit

--- components/3d/objects/planet.tsx
+++ components/3d/objects/planet.tsx
@@ -6,42 +6,44 @@ import MercuryModel from "./planet_models/Mercury";
 import NeptuneModel from "./planet_models/Neptune";
 import SaturnModel from "./planet_models/Saturn";
 import UranusModel from "./planet_models/Uranus";
-import { SCALE_FACTOR_ORBIT } from "@/lib/constants";
+import { Badge } from "@/components/ui/badge"
+import { SCALE_FACTOR_OBJECT, SCALE_FACTOR_ORBIT } from "@/lib/constants";
 import { Html } from "@react-three/drei";
-import { get_position_at_selected_date } from "@/lib/math";
+import { get_orbit_points, get_position_at_selected_date } from "@/lib/math";
+import { Orbit } from "./orbit";
 
-const selected_date = new Date("2023-10-01T00:00:00Z");
+type PlanetProps = {
+  name: string;
+  radius: number;
+  distance_from_sun: number;
+  color: string;
+  eccentricity: number;
+  inclination: number;
+  argument_of_periapsis: number;
+  longitude_of_ascending_node: number;
+  mean_anomaly: number;
+  mean_motion: number;
+  epoch: number;
+  selected_date: Date;
+};
 
-export default function Planet(
-  name: string,
-  radius: number, // kilometers
-  distance_from_sun: number, // kilometers
-  color: string,
-  eccentricity: number,
-  inclination: number, // radians
-  argument_of_periapsis: number, // radians
-  longitude_of_ascending_node: number, // radians
-  mean_anomaly: number, // radians
-  mean_motion: number,
-  epoch: number, // julian date
-) {
-  
+export default function Planet(planet: PlanetProps) {
   const planet_position = useMemo(() => {
     return get_position_at_selected_date(
-      distance_from_sun,
-      eccentricity,
-      mean_motion,
-      inclination,
-      argument_of_periapsis,
-      longitude_of_ascending_node,
-      mean_anomaly,
-      epoch,
-      selected_date
+      planet.distance_from_sun,
+      planet.eccentricity,
+      planet.mean_motion,
+      planet.inclination,
+      planet.argument_of_periapsis,
+      planet.longitude_of_ascending_node,
+      planet.mean_anomaly,
+      planet.epoch,
+      planet.selected_date
     );
-  }, [selected_date]);
+  }, [planet.selected_date]);
 
   const planet_model = useMemo(() => {
-    switch (name) {
+    switch (planet.name) {
       case "Earth":
         return <EarthModel />;
       case "Mars":
@@ -59,26 +61,51 @@ export default function Planet(
       default:
         return null;
     }
-  }, [name]);
+  }, [planet.name]);
+
+  const orbit_points = useMemo(() => {
+    return get_orbit_points(planet.distance_from_sun, planet.eccentricity, planet.inclination, planet.argument_of_periapsis, planet.longitude_of_ascending_node)
+  }, []);
 
   return (
     <>
-      <group position={[100, 200, 300]}>
-        <EarthModel
-          scale={[
-            radius * SCALE_FACTOR_ORBIT,
-            radius * SCALE_FACTOR_ORBIT,
-            radius * SCALE_FACTOR_ORBIT,
-          ]}
-        />
+      <group position={planet_position}>
         <Html
           center
           zIndexRange={[0, 0]}
-          className="group relative transition-all cursor-pointer"
+          className="group relative transition-all cursor-pointer "
         >
-          <p>{name}</p>
+          <Badge className="absolute -translate-x-1/2 bottom-3 ">{planet.name}</Badge>
+           <span className="absolute border-2 border-white w-4 h-4 group-hover:w-6 group-hover:h-6 rounded-full -translate-x-1/2 -translate-y-1/2 transition-all"
+           style={{ backgroundColor: planet.color }}></span>
         </Html>
       </group>
+      <Orbit points={orbit_points}/>
+
     </>
   );
-};
+}
+{
+  /*
+
+      <mesh>
+        <sphereGeometry
+          args={[
+            planet.radius * SCALE_FACTOR_OBJECT,
+            16,
+            16,
+          ]}
+        />
+        <meshStandardMaterial color={planet.color || "orange"} />
+      </mesh>
+
+
+
+        <EarthModel
+          scale={[
+            planet.radius * SCALE_FACTOR_ORBIT,
+            planet.radius * SCALE_FACTOR_ORBIT,
+            planet.radius * SCALE_FACTOR_ORBIT,
+          ]}
+        /> */
+}
