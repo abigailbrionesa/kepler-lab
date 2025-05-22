5eecfd29562623d567dcdf25bc4f45f4614517ce
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-05-22 05:30:26 -0500

    planets

---
+++ components/3d/objects/planets.tsx
@@ -0,0 +1,29 @@
+import { PLANET_DATA } from '@/lib/planet-data';
+
+const Planets = () => {
+  return (
+    <>
+      {PLANET_DATA.map((planet, index) => (
+        <Planet
+          key={index}
+          model = {planet.name + "Model"}
+          distance={planet.distanceFromSun}
+          radius={planet.diameter / 2}
+          ringColor={planet.color}
+          name={planet.name}
+          eccentricity={planet.orbitalEccentricity}
+          inclination={planet.orbitalInclination}
+          argumentPeriapsis={
+            planet.longitudePerihelion - planet.longitudeAscendingNode
+          }
+          ascendingNode={planet.longitudeAscendingNode}
+          meanAnomaly={planet.meanLongitude - planet.longitudePerihelion}
+          orbitalPeriod={planet.orbitalPeriod}
+          epoch={planet.epoch}
+        />
+))}
+    </>
+  );
+};
+
+export default Planets;
