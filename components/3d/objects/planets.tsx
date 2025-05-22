11bbb12a22f0d157a43a3e343370e94a480973b9
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-05-22 15:32:10 -0500

    pass props from supabase

--- components/3d/objects/planets.tsx
+++ components/3d/objects/planets.tsx
@@ -1,27 +1,27 @@
+import type { Planet as PlanetType } from "@prisma/client";
+import Planet from "./planet";
 
-export function Planets(planets_data) {
-  console.log(planets_data)
+export function Planets({planets_data, selected_date}: {planets_data : PlanetType[]; selected_date: Date} ) {
+  
   return (
     <>
-       {/* {planets.map((planet, index) => (
+       {planets_data.map((planet, index) => (
         <Planet
-          key={index}
-          model = {planet.name + "Model"}
-          distance={planet.distanceFromSun}
-          radius={planet.diameter / 2}
-          ringColor={planet.color}
+          key={planet.index}
           name={planet.name}
-          eccentricity={planet.orbitalEccentricity}
-          inclination={planet.orbitalInclination}
-          argumentPeriapsis={
-            planet.longitudePerihelion - planet.longitudeAscendingNode
-          }
-          ascendingNode={planet.longitudeAscendingNode}
-          meanAnomaly={planet.meanLongitude - planet.longitudePerihelion}
-          orbitalPeriod={planet.orbitalPeriod}
+          radius={planet.radius_km}
+          distance_from_sun={planet.distance_from_sun_km}
+          color={planet.color}
+          eccentricity={planet.eccentricity}
+          inclination={planet.inclination_rad}
+          argument_of_periapsis={planet.argument_of_periapsis_rad}
+          longitude_of_ascending_node={planet.longitude_ascending_node_rad}
+          mean_anomaly={planet.mean_anomaly_rad}
+          mean_motion={planet.mean_motion_rad}
           epoch={planet.epoch}
-        /> */}
-
+          selected_date={selected_date} 
+        />
+       ))}
     </>
   );
 };
