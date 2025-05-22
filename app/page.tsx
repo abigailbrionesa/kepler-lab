bc50159e008d8eec92f8ea5bf18320a4adc5589e
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-05-22 15:31:11 -0500

    add selecteddateprovider

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,32 @@
-import MainContent from "./main-content"
-import { AppSidebar } from "@/components/panel/app-sidebar"
-import { RightSidebar } from "@/components/panel/right-sidebar"
-import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
-import { SelectedCategoryProvider } from "@/context/selected-category-context"
-import { ViewConfigProvider } from "@/context/view-config-context"
-import { RightSidebarProvider } from "@/context/right-sidebar-context"
-import { getPlanets } from "./actions/getPlanets"
+import MainContent from "./main-content";
+import { AppSidebar } from "@/components/panel/app-sidebar";
+import { RightSidebar } from "@/components/panel/right-sidebar";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { SelectedCategoryProvider } from "@/context/selected-category-context";
+import { ViewConfigProvider } from "@/context/view-config-context";
+import { RightSidebarProvider } from "@/context/right-sidebar-context";
+import { SelectedDateProvider } from "@/context/view-selected-date";
+import { getPlanets } from "./actions/getPlanets";
 
 export default async function Page() {
-  const planets_data = await getPlanets()
-  if (!planets_data) return null
+  const planets_data = await getPlanets();
+
+  if (!planets_data) return null;
   return (
     <SelectedCategoryProvider>
-      <ViewConfigProvider>
-        <RightSidebarProvider>
-          <SidebarProvider>
-            <AppSidebar variant="inset" />
-            <SidebarInset>
-              <MainContent {...planets_data} />
-            </SidebarInset>
-            <RightSidebar />
-          </SidebarProvider>
-        </RightSidebarProvider>
-      </ViewConfigProvider>
+      <SelectedDateProvider>
+        <ViewConfigProvider>
+          <RightSidebarProvider>
+            <SidebarProvider>
+              <AppSidebar variant="inset" />
+              <SidebarInset>
+                <MainContent planets_data={planets_data} />
+              </SidebarInset>
+              <RightSidebar />
+            </SidebarProvider>
+          </RightSidebarProvider>
+        </ViewConfigProvider>
+      </SelectedDateProvider>
     </SelectedCategoryProvider>
-  )
+  );
 }
