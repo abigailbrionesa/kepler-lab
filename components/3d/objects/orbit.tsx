67c52cb7e360b26ca599e6ec0c430083ae2bb0af
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-05-22 15:31:41 -0500

    create orbit

---
+++ components/3d/objects/orbit.tsx
@@ -0,0 +1,17 @@
+import { Line } from '@react-three/drei';
+import { Vector3 } from 'three';
+
+interface OrbitProps {
+  points: Vector3[];
+}
+
+export const Orbit: React.FC<OrbitProps> = ({ points }) => {
+  return (
+    <Line
+      points={points}
+      color="white"
+      lineWidth={2}
+      dashed={false}
+    />
+  );
+};
