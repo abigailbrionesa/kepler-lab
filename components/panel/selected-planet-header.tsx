f322b9374232af5490d3da46423a91caf51b3988
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-06-23 08:00:50 -0500

    selected planet header

---
+++ components/panel/selected-planet-header.tsx
@@ -0,0 +1,40 @@
+import { useSelectedPlanet } from "@/context/view-selected-planet";
+import { Button } from "../ui/button";
+import { useIsObjectPivot } from "@/context/view-is-object-pivot";
+
+function SelectedPlanetHeader() {
+  const { selectedPlanet, setSelectedPlanet } = useSelectedPlanet();
+  const { isObjectPivot, togglePivot } = useIsObjectPivot();
+    console.log(isObjectPivot)
+  return (
+    <div className="w-full absolute z-10 p-2 shadow-lg">
+    
+      <div className="w-full flex justify-between p-1 custom-border items-center">
+
+       
+
+        {selectedPlanet}
+        <Button
+          variant={"ghost"}
+          onClick={() => setSelectedPlanet(undefined)}
+          className="float-right"
+        >
+          Cerrar
+        </Button>
+
+          <Button
+          variant="ghost"
+          size="sm"
+          className="gap-1.5"
+          onClick={togglePivot}
+        >
+          <span className="ml-2">
+            {isObjectPivot ? "Rotating around object" : "Rotating around sun"}
+          </span>
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default SelectedPlanetHeader;
