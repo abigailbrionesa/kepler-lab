e845872157e18f16e14dd02b79cf4259c80a1d81
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-06-23 06:13:52 -0500

    order contexts

--- app/page.tsx
+++ app/page.tsx
@@ -7,26 +7,31 @@ import { ViewConfigProvider } from "@/context/view-config-context";
 import { RightSidebarProvider } from "@/context/right-sidebar-context";
 import { SelectedDateProvider } from "@/context/view-selected-date";
 import { getPlanets } from "./actions/getPlanets";
+import { SelectedPlanetProvider } from "@/context/view-selected-planet";
 
 export default async function Page() {
   const planets_data = await getPlanets();
 
   if (!planets_data) return null;
   return (
-    <SelectedCategoryProvider>
-      <SelectedDateProvider>
+    <SidebarProvider>
+      <RightSidebarProvider>
         <ViewConfigProvider>
-          <RightSidebarProvider>
-            <SidebarProvider>
-              <AppSidebar variant="inset" />
-              <SidebarInset>
-                <MainContent planets_data={planets_data} />
-              </SidebarInset>
-              <RightSidebar />
-            </SidebarProvider>
-          </RightSidebarProvider>
+          <SelectedCategoryProvider>
+            <SelectedDateProvider>
+              <SelectedPlanetProvider>
+                <AppSidebar variant="inset" />
+                <SidebarInset>
+                  <MainContent planets_data={planets_data} />
+                </SidebarInset>
+                <RightSidebar />
+              </SelectedPlanetProvider>
+            </SelectedDateProvider>
+          </SelectedCategoryProvider>
         </ViewConfigProvider>
-      </SelectedDateProvider>
-    </SelectedCategoryProvider>
+      </RightSidebarProvider>
+    </SidebarProvider>
+
+
   );
 }
