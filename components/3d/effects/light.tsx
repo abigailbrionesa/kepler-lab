16840a026eb146bf9fdbfdde340903bd5a9a0c02
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-06-23 06:14:21 -0500

    add more effects

--- components/3d/effects/light.tsx
+++ components/3d/effects/light.tsx
@@ -1,5 +1,8 @@
 import React, { memo } from 'react';
 import { Bloom, EffectComposer, Noise } from '@react-three/postprocessing';
+import { N8AO } from '@react-three/postprocessing';
+import { TiltShift2 } from '@react-three/postprocessing';
+import { ToneMapping } from '@react-three/postprocessing';
 
 const Light: React.FC = memo(() => {
   return (
@@ -9,10 +12,13 @@ const Light: React.FC = memo(() => {
       <EffectComposer>
         <Bloom
           mipmapBlur
-          luminanceThreshold={0.6}
+          luminanceThreshold={0.2}
           intensity={0.3}
           luminanceSmoothing={1}
         />
+
+      <TiltShift2 samples={4} blur={0.04} />
+      <ToneMapping />
       </EffectComposer>
     </>
   );
