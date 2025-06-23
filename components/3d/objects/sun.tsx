c94c551a5572b6518f33f2187e0819f61e7f33cd
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-06-23 16:56:17 -0500

    add sunref to registered refs

--- components/3d/objects/sun.tsx
+++ components/3d/objects/sun.tsx
@@ -1,16 +1,28 @@
+"use client";
+
+import { useRef, useEffect } from "react";
+import { Object3D } from "three";
+import { useOccludableRefs } from "@/context/occludable-refs-context";
 import { SUN_RADIUS } from "@/lib/constants";
 
 const Sun = () => {
+  const meshRef = useRef<Object3D>(null);
+  const { registerRef, unregisterRef } = useOccludableRefs();
 
-    return (
-        <mesh position={[0, 0, 0]}>
-            <sphereGeometry args={[SUN_RADIUS * 1, 32, 32]} />
-            <meshStandardMaterial
-                emissive="orange"
-                emissiveIntensity={20}
-            />
-        </mesh>
-    );
+  useEffect(() => {
+    registerRef(meshRef);
+    return () => {
+      unregisterRef(meshRef);
+      console.log("unregistering sun")
+    };
+  }, [registerRef, unregisterRef]);
+
+  return (
+    <mesh ref={meshRef} position={[0, 0, 0]}>
+      <sphereGeometry args={[SUN_RADIUS * 1, 32, 32]} />
+      <meshStandardMaterial emissive="orange" emissiveIntensity={20} />
+    </mesh>
+  );
 };
 
 export default Sun;
