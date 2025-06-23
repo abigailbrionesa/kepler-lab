2dcfe3418690da9b0ce0b92b614f82b00ef80922
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-06-23 06:15:17 -0500

    scale?

--- components/3d/objects/planets.tsx
+++ components/3d/objects/planets.tsx
@@ -1,7 +1,48 @@
-import type { Planet as PlanetType } from "@prisma/client";
 import Planet from "./planet";
+import type { PlanetType } from "@/context/view-selected-planet";
 
-export function Planets({planets_data, selected_date}: {planets_data : PlanetType[]; selected_date: Date} ) {
+export type PlanetData = {
+  id: number;
+  name: PlanetType;
+  mass_kg: number;
+  diameter_km: number;
+  radius_km: number;
+  density_kg_m3: number;
+  gravity_m_s2: number;
+  escape_velocity_km_s: number;
+  rotation_period_hours: number;
+  length_of_day_hours: number;
+  distance_from_sun_km: number;
+  distance_from_sun_au: number;
+  perihelion_km: number;
+  perihelion_au: number;
+  aphelion_km: number;
+  aphelion_au: number;
+  period_days: number;
+  velocity_km_s: number;
+  inclination_deg: number;
+  inclination_rad: number;
+  eccentricity: number;
+  obliquity_to_orbit_deg: number;
+  obliquity_to_orbit_rad: number;
+  mean_temperature_c: number;
+  longitude_ascending_node_deg: number;
+  longitude_ascending_node_rad: number;
+  longitude_perihelion_deg: number;
+  longitude_perihelion_rad: number;
+  argument_of_periapsis_rad: number;
+  mean_motion_rad: number;
+  mean_longitude_deg: number;
+  mean_longitude_rad: number;
+  mean_anomaly_deg: number;
+  mean_anomaly_rad: number;
+  number_of_moons: number;
+  epoch: number;
+  color: string;
+  index: number;
+};
+
+export function Planets({planets_data, selected_date}: {planets_data : PlanetData[]; selected_date: Date} ) {
   
   return (
     <>
@@ -9,7 +50,7 @@ export function Planets({planets_data, selected_date}: {planets_data : PlanetTyp
         <Planet
           key={planet.index}
           name={planet.name}
-          radius={planet.radius_km}
+          radius={planet.radius_km * 0.009}
           distance_from_sun={planet.distance_from_sun_km}
           color={planet.color}
           eccentricity={planet.eccentricity}
