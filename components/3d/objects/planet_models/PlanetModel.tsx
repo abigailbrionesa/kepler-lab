d87758acebd357c98643f5dcf6dbdacc52760d6f
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-06-23 06:16:14 -0500

    planet model based on name

---
+++ components/3d/objects/planet_models/PlanetModel.tsx
@@ -0,0 +1,38 @@
+"use client"
+import type { PlanetType } from "@/context/view-selected-planet";
+import { EarthModel } from "./Earth";
+import MarsModel from "./Mars";
+import JupiterModel from "./Jupiter";
+import MercuryModel from "./Mercury";
+import NeptuneModel from "./Neptune";
+import SaturnModel from "./Saturn";
+import UranusModel from "./Uranus";
+import VenusModel from "./Venus";
+
+type PlanetModelProps = {
+  name: PlanetType;
+  scale: number;
+};
+
+export default function PlanetModel({ name, scale }: PlanetModelProps) {
+  switch (name) {
+    case 'Earth':
+      return <EarthModel scale={scale} />;
+    case 'Mars':
+      return <MarsModel scale={scale} />;
+    case 'Jupiter':
+      return <JupiterModel scale={scale} />;
+    case 'Venus':
+      return <VenusModel scale={scale} />;
+    case 'Mercury':
+      return <MercuryModel scale={scale} />;
+    case 'Neptune':
+      return <NeptuneModel scale={scale} />;
+    case 'Saturn':
+      return <SaturnModel scale={scale} />;
+    case 'Uranus':
+      return <UranusModel scale={scale} />;
+    default:
+      return null;
+  }
+}
