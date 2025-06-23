3771ec4009e832410053ccc1539e8151310e8cf5
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-06-23 09:04:35 -0500

    do not get selecteddate here

--- components/3d/solar-system.tsx
+++ components/3d/solar-system.tsx
@@ -6,7 +6,6 @@ import Stars from "./objects/stars";
 import { Light } from "./effects/light";
 import Planets from "./objects/planets";
 import { SpaceControls } from "./effects/orbit-controls";
-import { useSelectedDate } from "@/context/view-selected-date";
 import { useSelectedPlanet } from "@/context/view-selected-planet";
 import { cn } from "@/lib/utils";
 import SelectedPlanetHeader from "../panel/selected-planet-header";
@@ -16,7 +15,6 @@ import type { PlanetData } from "./objects/planets";
 export default function SolarSystem() {
   const planets_data = planets_json as PlanetData[];
 
-  const { selectedDate } = useSelectedDate();
   const { selectedPlanet,  } = useSelectedPlanet();
 
   return (
@@ -41,22 +39,10 @@ export default function SolarSystem() {
           <Sun />
           <SpaceControls />
           <Stars number={80000} size={3} />
-          <Planets planets_data={planets_data} selected_date={selectedDate} />
+          <Planets planets_data={planets_data} />
           <Light />
         </Canvas>
       </div>
     </Suspense>
   );
-}
-
-{
-  /*   */
-}
-
-{
-  /* <Grid />
-            <Sun />
-            <Planets />
-            <Stars number={80000} size={3} />
-            <Light />*/
 }
