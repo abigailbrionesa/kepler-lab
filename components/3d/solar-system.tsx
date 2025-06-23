211e5c36223ecb300cddeedc45956b7283ab8628
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-06-23 06:14:09 -0500

    add planet menu

--- components/3d/solar-system.tsx
+++ components/3d/solar-system.tsx
@@ -1,20 +1,35 @@
+"use client"
 import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import Sun from "./objects/sun";
 import Stars from "./objects/stars";
 import Light from "./effects/light";
-import type { Planet } from "@prisma/client";
+import type { PlanetData } from "./objects/planets";
 import Planets from "./objects/planets";
 import { SpaceControls } from "./effects/orbit-controls";
 import { useSelectedDate } from "@/context/view-selected-date";
+import { useSelectedPlanet } from "@/context/view-selected-planet";
+import { cn } from "@/lib/utils";
+import { Button } from "../ui/button";
 
-export default function SolarSystem({planets_data}: {planets_data: Planet[]}) {
+export default function SolarSystem({ planets_data }: { planets_data: PlanetData[] }) {
 
   const { selectedDate } = useSelectedDate();
+  const { selectedPlanet, setSelectedPlanet } = useSelectedPlanet();
 
   return (
     <Suspense fallback={<div>Loading solar system...</div>}>
-      <div className="w-full h-full bg-black z-0">
+      <div className={cn("w-full h-full text-white custom-border relative bg-black z-0", selectedPlanet ? "" : "")}>
+
+        {selectedPlanet && (
+          <div className="w-full absolute z-10 p-2 shadow-lg">
+            <div className="w-full flex justify-between p-1 custom-border items-center">
+            {selectedPlanet}
+            <Button variant={"ghost"} onClick={() => setSelectedPlanet(undefined)} className="float-right">Cerrar</Button>
+            </div>
+          </div>
+        )}
+
         <Canvas
           gl={{ antialias: false }}
           camera={{
@@ -25,9 +40,9 @@ export default function SolarSystem({planets_data}: {planets_data: Planet[]}) {
           }}
         >
           <Sun />
-          <SpaceControls/>
+          <SpaceControls />
           <Stars number={80000} size={3} />
-            <Planets planets_data={planets_data} selected_date={selectedDate} />
+          <Planets planets_data={planets_data} selected_date={selectedDate} />
           <Light />
         </Canvas>
       </div>
@@ -35,10 +50,10 @@ export default function SolarSystem({planets_data}: {planets_data: Planet[]}) {
   )
 }
 
- {/*   */}
+{/*   */ }
 
 
-   {/* <Grid />
+{/* <Grid />
             <Sun />
             <Planets />
             <Stars number={80000} size={3} />
