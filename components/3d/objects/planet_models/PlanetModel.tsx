349e8ace9ca35d634a52b3c0dd20181189aa49aa
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-06-23 08:00:43 -0500

    correct imports

--- components/3d/objects/planet_models/PlanetModel.tsx
+++ components/3d/objects/planet_models/PlanetModel.tsx
@@ -2,10 +2,10 @@
 import type { PlanetType } from "@/context/view-selected-planet";
 import { EarthModel } from "./Earth";
 import MarsModel from "./Mars";
-import JupiterModel from "./Jupiter";
+import { JupiterModel } from "./Jupiter";
 import MercuryModel from "./Mercury";
 import NeptuneModel from "./Neptune";
-import SaturnModel from "./Saturn";
+import { SaturnModel } from "./Saturn";
 import UranusModel from "./Uranus";
 import VenusModel from "./Venus";
 
