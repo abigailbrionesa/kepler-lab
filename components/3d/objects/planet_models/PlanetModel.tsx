1865357d20e8d488f8de3baf245ed77e19a10d01
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-06-24 05:42:13 -0500

    model = planet.name

--- components/3d/objects/planet_models/PlanetModel.tsx
+++ components/3d/objects/planet_models/PlanetModel.tsx
@@ -1,45 +1,46 @@
 "use client"
 import type { PlanetType } from "@/context/view-selected-planet";
 import { EarthModel } from "./Earth";
-import MarsModel from "./Mars";
+import { MarsModel } from "./Mars";
 import { JupiterModel } from "./Jupiter";
-import MercuryModel from "./Mercury";
+import { MercuryModel } from "./Mercury";
 import { NeptuneModel } from "./Neptune";
 import { SaturnModel } from "./Saturn";
-import UranusModel from "./Uranus";
-import VenusModel from "./Venus";
+import { UranusModel } from "./Uranus";
+import { VenusModel } from "./Venus";
 import { forwardRef } from "react";
+import type { Mesh } from "three";
 
 type PlanetModelProps = {
   name: PlanetType;
   scale: number;
-  visible?: boolean; 
-};
+} & React.ComponentProps<'group'>;  
 
-const PlanetModel = forwardRef<any, PlanetModelProps>(({ name, scale }, ref) => {
+const PlanetModel = forwardRef<Mesh, PlanetModelProps>((props, ref) => {
+  const { name, scale, ...rest } = props;
 
-  return <NeptuneModel ref={ref} scale={scale} />
-
-  {/* 
   switch (name) {
-    case 'Earth':
-      return <EarthModel ref={ref} scale={scale} />;
-    case 'Mars':
-      return <MarsModel ref={ref} scale={scale} />;
-    case 'Jupiter':
-      return <JupiterModel ref={ref} scale={scale} />;
-    case 'Venus':
-      return <VenusModel ref={ref} scale={scale} />;
-    case 'Mercury':
-      return <MercuryModel ref={ref} scale={scale} />;
-    case 'Neptune':
-      return <NeptuneModel ref={ref} scale={scale} />;
-    case 'Saturn':
-      return <SaturnModel ref={ref} scale={scale} />;
-    case 'Uranus':
-      return <UranusModel ref={ref} scale={scale} />;
+    case "Earth":
+      return <EarthModel ref={ref} scale={scale} {...rest} />;
+    case "Mars":
+      return <MarsModel ref={ref} scale={scale} {...rest} />;
+    case "Jupiter":
+      return <JupiterModel ref={ref} scale={scale} {...rest} />;
+    case "Venus":
+      return <VenusModel ref={ref} scale={scale} {...rest} />;
+    case "Mercury":
+      return <MercuryModel ref={ref} scale={scale} {...rest} />;
+    case "Neptune":
+      return <NeptuneModel ref={ref} scale={scale} {...rest} />;
+    case "Saturn":
+      return <SaturnModel ref={ref} scale={scale} {...rest} />;
+    case "Uranus":
+      return <UranusModel ref={ref} scale={scale} {...rest} />;
     default:
       return null;
-  }*/}
+  }
 });
+
+PlanetModel.displayName = "PlanetModel";
+
 export default PlanetModel;
