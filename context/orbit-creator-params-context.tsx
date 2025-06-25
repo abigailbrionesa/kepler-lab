fa85ad47ed2732d534af2b98ea7c8f73efb16613
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-06-25 13:46:37 -0500

    orbit creator params context

---
+++ context/orbit-creator-params-context.tsx
@@ -0,0 +1,57 @@
+"use client";
+import { createContext, useContext, useState } from "react";
+
+type OrbitalParams = {
+  name: string;
+  color: string;
+  semiMajorAxis: number;
+  eccentricity: number;
+  orbitalPeriod: number;
+  albedo: number;
+  magnitude: number;
+  diameter: number;
+  inclination: number;
+  argument_of_periapsis: number;
+  longitude_of_ascending_node: number;
+  mean_anomaly: number;
+  mean_motion: number;
+  epoch: number;
+  setParams: (params: Partial<OrbitalParams>) => void;
+};
+
+const OrbitalParamsContext = createContext<OrbitalParams | null>(null);
+
+export function OrbitCreatorParamsProvider({ children }: { children: React.ReactNode }) {
+  const [params, setParamsState] = useState<Omit<OrbitalParams, "setParams">>({
+    name: "Custom Planet",
+    color: "red",
+    semiMajorAxis: 0.47,
+    eccentricity: 0.2,
+    orbitalPeriod: 0.24,
+    albedo: 0.5,
+    magnitude: 10,
+    diameter: 6052 * 2,
+    inclination: 0.0592,
+    argument_of_periapsis: 0.9573,
+    longitude_of_ascending_node: 1.3381,
+    mean_anomaly: 3.1765,
+    mean_motion: 0.027958,
+    epoch: 2451545,
+  });
+
+  const setParams = (updates: Partial<OrbitalParams>) => {
+    setParamsState((prev) => ({ ...prev, ...updates }));
+  };
+
+  return (
+    <OrbitalParamsContext.Provider value={{ ...params, setParams }}>
+      {children}
+    </OrbitalParamsContext.Provider>
+  );
+}
+
+export const useOrbitalParams = () => {
+  const context = useContext(OrbitalParamsContext);
+  if (!context) throw new Error("useOrbitalParams?");
+  return context;
+};
