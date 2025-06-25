e52f319992d0da7dc34ac2824c2b6e5291c3eae5
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-06-25 13:46:03 -0500

    remove unnecessary conditionals

--- app/main-content.tsx
+++ app/main-content.tsx
@@ -1,13 +1,12 @@
 "use client";
-import { useSelectedCategory } from "@/context/selected-category-context";
 import SolarSystem from "@/components/3d/solar-system";
 import { SiteHeader } from "@/components/panel/site-header";
 import { useRef } from "react";
 import { useSelectedPlanet } from "@/context/view-selected-planet";
 import SelectedPlanetHeader from "@/components/panel/selected-planet-header";
 import ControlsPanel from "@/components/panel/controls-panel";
+import { OrbitalControlsPanel } from "@/components/panel/orbit-controls-panel";
 export default function MainContent() {
-  const { selectedCategory } = useSelectedCategory();
   const { selectedPlanet } = useSelectedPlanet();
   const containerRef = useRef<HTMLDivElement>(null);
   return (
@@ -16,7 +15,8 @@ export default function MainContent() {
       <div ref={containerRef} className="relative flex flex-1 flex-col">
         <div className="@container/main flex flex-1 flex-col">
           {selectedPlanet && <SelectedPlanetHeader />}
-          {selectedCategory && <ControlsPanel dragConstraints={containerRef} />}
+          {<ControlsPanel dragConstraints={containerRef} />}
+          {<OrbitalControlsPanel dragConstraints={containerRef} />}
           <div className="flex flex-1">
             <SolarSystem />
           </div>
