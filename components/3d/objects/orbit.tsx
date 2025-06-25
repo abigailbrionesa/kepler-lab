6af32541ef04b918520c544ec7e711f400fdc0a3
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-06-25 08:26:14 -0500

    less opacity

--- components/3d/objects/orbit.tsx
+++ components/3d/objects/orbit.tsx
@@ -1,18 +1,18 @@
-import { Line } from '@react-three/drei';
-import { Vector3 } from 'three';
+import { Line } from "@react-three/drei";
+import { Vector3 } from "three";
 
 interface OrbitProps {
   points: Vector3[];
 }
 
 export const Orbit: React.FC<OrbitProps> = ({ points }) => {
-
-
   return (
     <Line
       points={points}
       color="white"
       lineWidth={1}
+      opacity={0.2}
+      transparent={true}
       dashed={false}
     />
   );
