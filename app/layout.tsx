f2f6bce35a8f7eeb86df28bfb1662ea46dc4d595
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-06-24 19:27:56 -0500

    supress warning

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,11 +20,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${ibmPlexMono.variable} antialiased`}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="dark"
+          defaultTheme="system"
           enableSystem
           disableTransitionOnChange
         >
