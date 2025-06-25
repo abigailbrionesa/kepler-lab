3f91c7cc2720a5a3d7089ad06771e8bf53470ece
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-06-25 12:39:11 -0500

    add controlspanel

--- app/main-content.tsx
+++ app/main-content.tsx
@@ -1,11 +1,11 @@
 "use client";
 import { useSelectedCategory } from "@/context/selected-category-context";
 import SolarSystem from "@/components/3d/solar-system";
-import ControlPanel from "@/components/panel/control-panel";
 import { SiteHeader } from "@/components/panel/site-header";
 import { useRef } from "react";
 import { useSelectedPlanet } from "@/context/view-selected-planet";
 import SelectedPlanetHeader from "@/components/panel/selected-planet-header";
+import ControlsPanel from "@/components/panel/controls-panel";
 export default function MainContent() {
   const { selectedCategory } = useSelectedCategory();
   const { selectedPlanet } = useSelectedPlanet();
@@ -13,11 +13,10 @@ export default function MainContent() {
   return (
     <div className="flex flex-1 flex-col border-secondary border-1  rounded-2xl">
       <SiteHeader />
-<div ref={containerRef} className="relative flex flex-1 flex-col">
+      <div ref={containerRef} className="relative flex flex-1 flex-col">
         <div className="@container/main flex flex-1 flex-col">
-
-          {selectedCategory && <ControlPanel dragConstraints={containerRef} />}
           {selectedPlanet && <SelectedPlanetHeader />}
+          {selectedCategory && <ControlsPanel dragConstraints={containerRef} />}
           <div className="flex flex-1">
             <SolarSystem />
           </div>
