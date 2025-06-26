92c45d7dfaca0c2f79b650a70667a871f59a4a69
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-06-26 15:48:02 -0500

    selectedasteroidspkid context

---
+++ context/view-selected-asteroid-spkid.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { createContext, useContext, useState, type ReactNode } from "react";
+
+interface SelectedAsteroidSpkidType {
+  selectedAsteroidSpkid: string | undefined;
+  setSelectedAsteroidSpkid: (spkid: string | undefined) => void;
+}
+
+const SelectedAsteroidSpkidContext = createContext<SelectedAsteroidSpkidType>({
+  selectedAsteroidSpkid: undefined,
+  setSelectedAsteroidSpkid: () => {
+    throw new Error("setSelectedAsteroidSpkid must be used within a SelectedAsteroidSpkidProvider");
+  },
+});
+
+export const useSelectedAsteroidSpkid = () => useContext(SelectedAsteroidSpkidContext);
+
+export const SelectedAsteroidSpkidProvider = ({ children }: { children: ReactNode }) => {
+  const [selectedAsteroidSpkid, setSelectedAsteroidSpkid] = useState<string | undefined>(undefined);
+
+  return (
+    <SelectedAsteroidSpkidContext.Provider value={{ selectedAsteroidSpkid, setSelectedAsteroidSpkid }}>
+      {children}
+    </SelectedAsteroidSpkidContext.Provider>
+  );
+};
