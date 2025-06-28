95241a4c5d76bfc7ef10c215ddb095f5f47c31d9
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-06-28 15:26:39 -0500

    panel to select asteroids

---
+++ components/panel/asteroid-panel.tsx
@@ -0,0 +1,56 @@
+"use client";
+import {
+  Accordion,
+  AccordionItem,
+} from "@/components/ui/accordion";
+import DraggablePanel from "../ui/draggable-menu";
+import { AsteroidSelector } from "../ui/asteroid-selector";
+import { useAsteroids } from "@/context/asteroids-context";
+import DraggableMenuItem from "../ui/draggable-menu-item";
+import DeleteButton from "../ui/delete-button";
+
+export function AsteroidsPanel({
+  dragConstraints,
+}: {
+  dragConstraints: React.RefObject<HTMLDivElement | null>;
+}) {
+  const { asteroids, removeAsteroid } = useAsteroids();
+
+  return (
+    <DraggablePanel
+      dragConstraints={dragConstraints}
+      title="Select Asteroids"
+      position="left-80 top-5"
+      width="w-72"
+    >
+     <>
+  <AsteroidSelector className="py-4" />
+
+  {asteroids.length > 0 ? (
+    <>
+      <h3 className="text-sm font-medium">Selected Asteroids:</h3>
+      <Accordion type="multiple" className="w-full">
+        {asteroids.map((asteroid) => (
+          <AccordionItem value={asteroid.spkid} key={asteroid.spkid}>
+            <DraggableMenuItem
+              accordionValue={asteroid.spkid}
+              title={asteroid.full_name}
+            >
+              <DeleteButton
+                onClick={(e) => {
+                  e.stopPropagation();
+                  removeAsteroid(asteroid.spkid);
+                }}
+              />
+            </DraggableMenuItem>
+          </AccordionItem>
+        ))}
+      </Accordion>
+    </>
+  ) : (
+    <p className="text-sm pb-4">No asteroids selected yet.</p>
+  )}
+</>
+    </DraggablePanel>
+  );
+}
