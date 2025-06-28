0a92088e570c2e44e4bc704a792c986f58d8816a
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-06-28 07:32:05 -0500

    update with new context

--- components/3d/orbit-creator.tsx
+++ components/3d/orbit-creator.tsx
@@ -1,41 +1,35 @@
 "use client";
-import CelestialObject from "./objects/celestial_object";
 import { useOrbitalParams } from "@/context/orbit-creator-params-context";
+import Object from "./objects/object";
+import { useSelectedDate } from "@/context/view-selected-date";
+import { useCustomObjects } from "@/context/custom-objects-context";
 
 export default function OrbitCreator() {
-  const {
-    semiMajorAxis,
-    eccentricity,
-    orbitalPeriod,
-    albedo,
-    magnitude,
-    diameter,
-    inclination,
-    argument_of_periapsis,
-    longitude_of_ascending_node,
-    mean_anomaly,
-    mean_motion,
-    epoch,
-    name,
-    color,
-  } = useOrbitalParams();
+  const { selectedDate } = useSelectedDate();
+  const { objects } = useCustomObjects();
 
   return (
-    <CelestialObject
-      semiMajorAxis={semiMajorAxis}
-      eccentricity={eccentricity}
-      orbitalPeriod={orbitalPeriod}
-      albedo={albedo}
-      magnitude={magnitude}
-      diameter={diameter}
-      inclination={inclination}
-      argument_of_periapsis={argument_of_periapsis}
-      longitude_of_ascending_node={longitude_of_ascending_node}
-      mean_anomaly={mean_anomaly}
-      mean_motion={mean_motion}
-      epoch={epoch}
-      name={name}
-      color={color}
-    />
+    <>
+      {objects.map((obj) => (
+        <Object
+          key={obj.id}
+          type="NEA"
+          selectedDate={selectedDate}
+          objectParams={{
+            name: obj.name,
+            distance_from_sun: obj.semiMajorAxis,
+            color: "#ffffff",
+            eccentricity: obj.eccentricity,
+            inclination: obj.inclination,
+            argument_of_periapsis: obj.argument_of_periapsis,
+            longitude_of_ascending_node: obj.longitude_of_ascending_node,
+            mean_anomaly: obj.mean_anomaly,
+            mean_motion: obj.mean_motion,
+            epoch: obj.epoch,
+          }}
+          custom
+        />
+      ))}
+    </>
   );
 }
