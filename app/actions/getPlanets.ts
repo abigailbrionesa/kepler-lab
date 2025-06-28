0872ef8a58944fc4f9c3c25839be45b3c70ef5fd
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-06-28 16:56:04 -0500

    we are not using prisma

--- app/actions/getPlanets.ts
+++
@@ -1,8 +0,0 @@
-'use server'
-
-import prisma from "@/lib/prisma"
-import type { Planet } from "@prisma/client"
-
-export async function getPlanets(): Promise<Planet[]> {
-  return await prisma.planet.findMany()
-}
