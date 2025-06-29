b2f39f14ce49993ff80470f7adb560224a1ae58e
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-06-29 11:33:24 -0500

    move types to types

--- components/3d/objects/planets.tsx
+++ components/3d/objects/planets.tsx
@@ -1,47 +1,6 @@
 import Object from "./object";
-import type { PlanetType } from "@/context/view-selected-planet";
 import { useSelectedDate } from "@/context/view-selected-date";
-
-export type PlanetData = {
-  id: number;
-  name: PlanetType;
-  mass_kg: number;
-  diameter_km: number;
-  radius_km: number;
-  density_kg_m3: number;
-  gravity_m_s2: number;
-  escape_velocity_km_s: number;
-  rotation_period_hours: number;
-  length_of_day_hours: number;
-  distance_from_sun_km: number;
-  distance_from_sun_au: number;
-  perihelion_km: number;
-  perihelion_au: number;
-  aphelion_km: number;
-  aphelion_au: number;
-  period_days: number;
-  velocity_km_s: number;
-  inclination_deg: number;
-  inclination_rad: number;
-  eccentricity: number;
-  obliquity_to_orbit_deg: number;
-  obliquity_to_orbit_rad: number;
-  mean_temperature_c: number;
-  longitude_ascending_node_deg: number;
-  longitude_ascending_node_rad: number;
-  longitude_perihelion_deg: number;
-  longitude_perihelion_rad: number;
-  argument_of_periapsis_rad: number;
-  mean_motion_rad: number;
-  mean_longitude_deg: number;
-  mean_longitude_rad: number;
-  mean_anomaly_deg: number;
-  mean_anomaly_rad: number;
-  number_of_moons: number;
-  epoch: number;
-  color: string;
-  index: number;
-};
+import type { PlanetData } from "@/lib/types";
 
 export function Planets({ planets_data }: { planets_data: PlanetData[] }) {
   const { selectedDate } = useSelectedDate();
