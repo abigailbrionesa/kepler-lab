1f08317bbeade23eb72db03f0f30872778b51ddf
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-06-29 06:41:43 -0500

    add emissive texture

--- components/3d/objects/sun.tsx
+++ components/3d/objects/sun.tsx
@@ -1,26 +1,32 @@
-"use client";
-
-import { useRef, useEffect } from "react";
-import { Object3D } from "three";
+import { useRef, useEffect, useState } from "react";
+import { Object3D, TextureLoader } from "three";
 import { useOccludableRefs } from "@/context/occludable-refs-context";
 import { SUN_RADIUS } from "@/lib/constants";
-
+import { useLoader } from "@react-three/fiber";
+import { MeshStandardMaterial } from "three";
 const Sun = () => {
   const meshRef = useRef<Object3D>(null);
   const { registerRef, unregisterRef } = useOccludableRefs();
+
+  const sunTexture = useLoader(TextureLoader, "/textures/sun-texture.jpg");
 
   useEffect(() => {
     registerRef(meshRef);
     return () => {
       unregisterRef(meshRef);
-      console.log("unregistering sun")
     };
   }, [registerRef, unregisterRef]);
 
   return (
     <mesh ref={meshRef} position={[0, 0, 0]}>
-      <sphereGeometry args={[SUN_RADIUS * 1, 32, 32]} />
-      <meshStandardMaterial emissive="orange" emissiveIntensity={20} />
+      <sphereGeometry args={[SUN_RADIUS, 16, 16]} />
+      <meshStandardMaterial
+        map={sunTexture}
+        emissiveMap={sunTexture}
+        emissive={"#d49000"}
+        emissiveIntensity={3}
+        toneMapped={true}
+      />
     </mesh>
   );
 };
