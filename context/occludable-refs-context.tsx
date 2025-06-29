ba148ab8070ecdb445aea8a18e21f48ea6cd78af
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-06-29 06:42:16 -0500

    remove console logs

--- context/occludable-refs-context.tsx
+++ context/occludable-refs-context.tsx
@@ -2,8 +2,7 @@
 
 import { createContext, useContext, useRef } from "react";
 import { Object3D } from "three";
-import type { ReactNode } from "react";
-import type { RefObject } from "react";
+import type { ReactNode, RefObject } from "react";
 
 type OccludableRefsContextType = {
   refs: React.RefObject<RefObject<Object3D | null>[]>;
@@ -25,15 +24,9 @@ export const useOccludableRefs = () => {
 export const OccludableRefsProvider = ({ children }: { children: ReactNode }) => {
   const refs = useRef<RefObject<Object3D | null>[]>([]);
 
-  const logRefs = () => {
-    console.log("Current Refs:", refs.current.map(ref => ref.current));
-  };
-
   const registerRef = (ref: RefObject<Object3D | null>) => {
     if (!refs.current.includes(ref)) {
       refs.current.push(ref);
-      console.log("Registered Ref:", ref);
-      logRefs();
     }
   };
 
@@ -41,8 +34,6 @@ export const OccludableRefsProvider = ({ children }: { children: ReactNode }) =>
     const index = refs.current.indexOf(ref);
     if (index !== -1) {
       refs.current.splice(index, 1);
-      console.log("Unregistered Ref:", ref);
-      logRefs();
     }
   };
 
