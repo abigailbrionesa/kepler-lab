3549a0832d2cd4677c3b8d796941bca4b6d049af
abigail_br987 <abigailbrionesaranda@gmail.com>
2025-07-02 14:38:19 -0500

    add new getrandomcolor function

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function getRandomColor(): string {
+  const hue = Math.floor(Math.random() * 360);
+  return `hsl(${hue}, 100%, 50%)`;
+}
