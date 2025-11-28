e451760f14879caa5bca27748ebf6d7c19ca0495
Abigail Briones <abigailbrionesaranda@gmail.com>
2025-11-28 08:25:43 -0500

    typesafe (undefined)

--- searchWorker.js
+++ searchWorker.js
@@ -6,14 +6,14 @@ let allAsteroids = [];
 
 function applyFilters(asteroids, filters) {
   return asteroids.filter(asteroid => {
-    if (filters.minDistance && asteroid.a * 149597871 < filters.minDistance) return false;
-    if (filters.maxDistance && asteroid.a * 149597871 > filters.maxDistance) return false;
-    if (filters.minEccentricity && asteroid.e < filters.minEccentricity) return false;
-    if (filters.maxEccentricity && asteroid.e > filters.maxEccentricity) return false;
-    if (filters.minInclination && asteroid.i < filters.minInclination) return false;
-    if (filters.maxInclination && asteroid.i > filters.maxInclination) return false;
-    if (filters.minMagnitude && asteroid.H < filters.minMagnitude) return false;
-    if (filters.maxMagnitude && asteroid.H > filters.maxMagnitude) return false;
+    if (filters.minDistance !== undefined && asteroid.a * 149597871 < filters.minDistance) return false;
+    if (filters.maxDistance !== undefined && asteroid.a * 149597871 > filters.maxDistance) return false;
+    if (filters.minEccentricity !== undefined && asteroid.e < filters.minEccentricity) return false;
+    if (filters.maxEccentricity !== undefined && asteroid.e > filters.maxEccentricity) return false;
+    if (filters.minInclination !== undefined && asteroid.i < filters.minInclination) return false;
+    if (filters.maxInclination !== undefined && asteroid.i > filters.maxInclination) return false;
+    if (filters.minMagnitude !== undefined && asteroid.H < filters.minMagnitude) return false;
+    if (filters.maxMagnitude !== undefined && asteroid.H > filters.maxMagnitude) return false;
     return true;
   });
 }
@@ -40,8 +40,8 @@ self.onmessage = (e) => {
     });
 
     miniSearch.addAll(allAsteroids);
-    
-    self.postMessage({ 
+
+    self.postMessage({
       type: "READY",
       stats: {
         total: allAsteroids.length,
@@ -57,48 +57,48 @@ self.onmessage = (e) => {
     });
   }
 
-  if (type === "SEARCH") {
-    if (!miniSearch) {
-      self.postMessage({ type: "RESULTS", results: [], count: 0 });
-      return;
-    }
-
-    const { query = "", filters = {} } = payload;
-    let results = [];
-    
-    if (query.trim()) {
-      const searchResults = miniSearch.search(query);
-      const resultIds = new Set(searchResults.map(r => r.spkid));
-      results = allAsteroids.filter(a => resultIds.has(String(a.spkid)));
-    } else {
-      results = [...allAsteroids];
-    }
-
-    const filteredResults = applyFilters(results, filters);
-    
-    const sortedResults = filteredResults
-      .sort((a, b) => (a.condition_code || 9) - (b.condition_code || 9))
-      .map((a) => ({
-        spkid: String(a.spkid),
-        full_name: a.full_name,
-        condition_code: a.condition_code,
-        a: a.a,
-        e: a.e,
-        i: a.i,
-        H: a.H,
-      }));
-
-    self.postMessage({ 
-      type: "RESULTS", 
-      results: sortedResults,
-      count: filteredResults.length
-    });
+if (type === "SEARCH") {
+  if (!miniSearch) {
+    self.postMessage({ type: "RESULTS", results: [], count: 0 });
+    return;
   }
+
+  const { query = "", filters = {} } = payload;
+  let results = [];
+
+  if (query.trim()) {
+    const lowerQuery = query.toLowerCase();
+    results = allAsteroids.filter(a => a.full_name.toLowerCase().includes(lowerQuery));
+  } else {
+    results = [...allAsteroids];
+  }
+
+  const filteredResults = applyFilters(results, filters);
+
+  const sortedResults = filteredResults
+    .sort((a, b) => (a.condition_code || 9) - (b.condition_code || 9))
+    .map((a) => ({
+      spkid: String(a.spkid),
+      full_name: a.full_name,
+      condition_code: a.condition_code,
+      a: a.a,
+      e: a.e,
+      i: a.i,
+      H: a.H,
+    }));
+
+  self.postMessage({
+    type: "RESULTS",
+    results: sortedResults,
+    count: filteredResults.length
+  });
+}
+
 
   if (type === "FETCH_BY_SPKID") {
     const spkid = String(payload.spkid);
     const asteroid = asteroidsMap.get(spkid);
-    
+
     if (asteroid) {
       self.postMessage({ type: "ASTEROID_DATA", asteroid });
     } else {
